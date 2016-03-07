2a566588f5123cde01b71a06977621d3aac069f8
Anshul Sahni <anshul_sahni@live.com>
2016-03-07 14:38:20 +0530

    added filterMailProps helper for mail folders

---
+++ scripts/helpers/filterMailProps.jsx
@@ -0,0 +1,21 @@
+import _ from 'underscore';
+
+//helper function used by mail folder components to filter the mail properties to be passed on to maillist
+filterMailProps(mailList,properties) {
+  var mailsWithFilteredProps= _.map(mailList,(mail)=> {
+    var filteredObject= {};
+    _.each(properties,(prop)=> {
+      if(mail.hasOwnProperty(prop)) {
+        filteredObject[prop]=mail[prop];
+      }
+      else {
+        throw 'Property Listed in properties argument is not present in object passed';
+      }
+    });
+    return filteredObject;
+  });
+  return mailsWithFilteredProps;
+
+}
+
+export default filterMailProps;
