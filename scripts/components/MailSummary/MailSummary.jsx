7c507194e3da4b6c6224c34c85fd8937ba81c5bc
Anshul Sahni <anshul_sahni@live.com>
2016-03-07 16:49:51 +0530

    Added Mail Summary Component

---
+++ scripts/components/MailSummary/MailSummary.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { userRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
+
+var appHistory = userRouterHistory(createHashHistory)({ queryKey: false }); 
+
+class MailSummary extends React.Component {
+
+  constructor() {
+    super();
+    console.log(this);
+  }
+
+  renderSummary() {
+    return this.props.mail.subject.substring(0,20).trim() + '...';
+  }
+
+  redirectToMail(event) {
+    appHistory.push('/mail/'+this.props.mail.key);
+  }
+
+  renderRecepients(recipients) {
+    var names = '';
+    if(recipients[0]) {
+      names += recipients[0].name;
+      if(recipients[1]) {
+        names += ',' + recipients[1].name;
+        if(recipients[2]) {
+          names += '...';
+        }
+      }
+    }
+    return names;
+  }
+
+  render() {
+    return(
+        <tr onClick={this.redirectToMail}>
+          <td className='mdl-data-table__cell--non-numeric'>
+            <i className='material-icons'>
+              {this.props.mail.starred ? 'star' : 'star_border'}
+            </i>
+          </td>
+          <td className='mdl-data-table__cell--non-numeric'>
+            <strong>{this.props.mail.to ? this.renderRecepients(this.props.mail.to) : this.props.mail.from.name}</strong>
+          </td>
+          <td className='mdl-data-table__cell--non-numeric'>
+            {this.renderSummary()}
+          </td>
+          <td className='mdl-data-table__cell--non-numeric'>
+            {this.props.mail.sendingDate}
+          </td>
+        </tr>
+      );
+  }
+}
+
+export default MailSummary;
